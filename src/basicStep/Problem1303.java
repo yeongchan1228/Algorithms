52349bfe73a2b78e8c3bb7b21a446bf7e2ffe695
최영찬 <gks54410@gmail.com>
2022-01-28 23:58:55 +0900

    11. 2022/01/28 1문제 풀이.

---
+++ src/basicStep/Problem1303.java
@@ -0,0 +1,62 @@
+package basicStep;
+
+import java.util.Scanner;
+
+public class Problem1303 {
+
+    static char[][] map;
+    static boolean[][] visited;
+    static int N, M = 0;
+    static int count = 0;
+    static int result1, result2 = 0;
+
+    public static void dfs(char c, int n, int m){
+        visited[n][m] = true;
+        count++;
+        if(n - 1 >= 0 && map[n-1][m] == c && !visited[n-1][m]){ // 위 탐색
+            dfs(c, n-1, m);
+        }
+        if(n + 1 <= N-1 && map[n+1][m] == c && !visited[n+1][m]){ // 아래 탐색
+            dfs(c, n+1, m);
+        }
+        if(m + 1 <= M-1 && map[n][m+1] == c && !visited[n][m+1]){ // 오른쪽 탐색
+            dfs(c, n, m+1);
+        }
+        if(m - 1 >= 0 && map[n][m-1] == c && !visited[n][m-1]){ // 왼쪽 탐색
+            dfs(c, n, m-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt(); // 가로
+        M = in.nextInt(); // 세로
+
+        map = new char[N][M];
+        visited = new boolean[N][M];
+
+        for(int i = 0; i < N; i++){
+            String str = in.next();
+            for(int j =0; j < M; j++){
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(map[i][j] == 'W' && !visited[i][j]){
+                    dfs('W', i, j);
+                    result1 += count * count;
+                    count = 0;
+                }else if(map[i][j] == 'B' && !visited[i][j]){
+                    dfs('B', i, j);
+                    result2 += count * count;
+                    count = 0;
+                }
+            }
+        }
+
+        System.out.print(result1 + " "+ result2);
+    }
+}
