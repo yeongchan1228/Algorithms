6433fb1f99ae83fe1b62ffee7d16f9037c8796fe
최영찬 <gks54410@gmail.com>
2022-01-29 00:29:49 +0900

    12. 2022/01/29 1문제 수정.

--- src/basicStep/Problem1303.java
+++ src/basicStep/Problem1303.java
@@ -13,16 +13,17 @@ public class Problem1303 {
     public static void dfs(char c, int n, int m){
         visited[n][m] = true;
         count++;
-        if(n - 1 >= 0 && map[n-1][m] == c && !visited[n-1][m]){ // 위 탐색
+
+        if(n - 1 > -1 && map[n-1][m] == c && !visited[n-1][m]){ // 위 탐색
             dfs(c, n-1, m);
         }
-        if(n + 1 <= N-1 && map[n+1][m] == c && !visited[n+1][m]){ // 아래 탐색
+        if(n + 1 < N && map[n+1][m] == c && !visited[n+1][m]){ // 아래 탐색
             dfs(c, n+1, m);
         }
-        if(m + 1 <= M-1 && map[n][m+1] == c && !visited[n][m+1]){ // 오른쪽 탐색
+        if(m + 1 < M && map[n][m+1] == c && !visited[n][m+1]){ // 오른쪽 탐색
             dfs(c, n, m+1);
         }
-        if(m - 1 >= 0 && map[n][m-1] == c && !visited[n][m-1]){ // 왼쪽 탐색
+        if(m - 1 > -1 && map[n][m-1] == c && !visited[n][m-1]){ // 왼쪽 탐색
             dfs(c, n, m-1);
         }
     }
@@ -30,8 +31,8 @@ public class Problem1303 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        N = in.nextInt(); // 가로
         M = in.nextInt(); // 세로
+        N = in.nextInt(); // 가로
 
         map = new char[N][M];
         visited = new boolean[N][M];
