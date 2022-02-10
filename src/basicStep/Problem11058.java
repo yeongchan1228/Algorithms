d93c63f1cb88b555b370e9263b78f3121e179dab
최영찬 <gks54410@gmail.com>
2022-02-10 23:24:36 +0900

    21. 2022/02/10 1문제 풀이.

---
+++ src/basicStep/Problem11058.java
@@ -0,0 +1,30 @@
+package basicStep;
+
+import java.util.Scanner;
+
+public class Problem11058 { // 크리보드
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        long[] dp = new long[101];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        dp[4] = 4;
+        dp[5] = 5;
+        dp[6] = 6;
+
+
+        for(int i = 7; i <= N; i++){
+            dp[i] = dp[i - 1] + 1;
+            for(int j = 0; j < i - 3; j++){
+                dp[i] = Math.max(dp[i], dp[i - (j + 3)] * (j + 2));
+            }
+        }
+
+        System.out.print(dp[N]);
+    }
+}
