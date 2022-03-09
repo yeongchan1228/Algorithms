588c66d489c7d311f9cb160959fe010bae1d4690
최영찬 <gks54410@gmail.com>
2022-03-09 15:24:08 +0900

    43. 2022/03/09 백준 1문제 풀이.
    Signed-off-by: 최영찬 <gks54410@gmail.com>

---
+++ src/basicStep/Problem2193.java
@@ -0,0 +1,25 @@
+package basicStep;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2193 { // 이친수
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(in.readLine());
+
+        long[] dp = new long[N + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
