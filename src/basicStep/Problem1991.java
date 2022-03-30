e1a08bdf13a047fa4293f7a64b0503251b3b14c3
최영찬 <gks54410@gmail.com>
2022-03-30 14:20:44 +0900

    Feat: Node 배열로 변경
    Signed-off-by: 최영찬 <gks54410@gmail.com>

--- src/basicStep/Problem1991.java
+++ src/basicStep/Problem1991.java
@@ -1,12 +1,10 @@
 package basicStep;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Problem1991 { // 트리 순회
 
-    static List<Node>[] nodes;
+    static Node[] nodes;
     static StringBuilder sb = new StringBuilder();
     static int N;
 
@@ -15,18 +13,15 @@ public class Problem1991 { // 트리 순회
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(in.readLine());
-        nodes = new ArrayList[N + 1];
-
-        for(int i=1; i<N+1; i++) {
-            nodes[i] = new ArrayList<>();
-        }
+        nodes = new Node[N + 1];
 
         for(int i=1; i<N+1; i++) {
             String[] line = in.readLine().split(" ");
             int data = line[0].charAt(0) -'A'+1;
             int left = line[1].charAt(0) -'A'+1;
             int right = line[2].charAt(0) -'A'+1;
-            nodes[data].add(new Node(right, left));
+            Node newNode = new Node(right, left);
+            nodes[data] = newNode;
         }
 
         preorder(1);
@@ -40,45 +35,48 @@ public class Problem1991 { // 트리 순회
         out.close();
     }
 
-    // 전위 순회 root > left > right
     static void preorder(int start) {
-        for(Node node : nodes[start]) {
-            int l = node.left;
-            int r = node.right;
+        Node node = nodes[start];
+        int left = node.getLeft();
+        int right = node.getRight();
 
-            sb.append((char)(start+'A'-1));
-            if(l != -18) preorder(l);
-            if(r != -18) preorder(r);
-        }
+        sb.append((char)(start+'A'-1));
+        if(left != -18) preorder(left);
+        if(right != -18) preorder(right);
+
     }
 
-    // 중위 순회 left > root > right
     static void inorder(int start) {
-        for(Node node : nodes[start]) {
-            int l = node.left;
-            int r = node.right;
+        Node node = nodes[start];
+        int left = node.getLeft();
+        int right = node.getRight();
 
-            if(l != -18) inorder(l);
-            sb.append((char)(start+'A'-1));
-            if(r != -18) inorder(r);
-        }
+        if(left != -18) inorder(left);
+        sb.append((char)(start+'A'-1));
+        if(right != -18) inorder(right);
     }
 
-    // 후위 순회 left > right > root
     static void postorder(int start) {
-        for(Node node : nodes[start]) {
-            int l = node.left;
-            int r = node.right;
+        Node node = nodes[start];
+        int left = node.getLeft();
+        int right = node.getRight();
 
-            if(l != -18) postorder(l);
-            if(r != -18) postorder(r);
-            sb.append((char)(start+'A'-1));
-        }
+        if(left != -18) postorder(left);
+        if(right != -18) postorder(right);
+        sb.append((char)(start+'A'-1));
     }
 
     static class Node{
         private int right;
         private int left;
+
+        public int getRight() {
+            return right;
+        }
+
+        public int getLeft() {
+            return left;
+        }
 
         public Node(int right, int left) {
             this.right = right;
