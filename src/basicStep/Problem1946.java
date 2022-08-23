f30a1583f918753a50301bc4a37da63f17e7c695
최영찬 <gks54410@gmail.com>
2022-08-23 12:53:33 +0900

    Feat: 백준 1946번 풀이
    Signed-off-by: 최영찬 <gks54410@gmail.com>

--- src/basicStep/Problem1946.java
+++ src/basicStep/Problem1946.java
@@ -1,49 +1,56 @@
 package basicStep;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Problem1946 { // 통나무 건너뛰기
+public class Problem1946 { // 신입 사원
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(in.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(in.readLine());
-            int[] arr = new int[num];
+        int n = Integer.parseInt(in.readLine());
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int cnt = Integer.parseInt(in.readLine());
 
-            StringTokenizer st = new StringTokenizer(in.readLine());
-            for (int j = 0; j < num; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
+            int[] arr = new int[cnt + 1];
+            for (int j = 0; j < cnt; j++) {
+                StringTokenizer st = new StringTokenizer(in.readLine());
+                arr[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(arr);
+//            int[][] grades = new int[cnt][2];
+//
+//            for (int j = 0; j < cnt; j++) {
+//                StringTokenizer st = new StringTokenizer(in.readLine());
+//                grades[j][0] = Integer.parseInt(st.nextToken());
+//                grades[j][1] = Integer.parseInt(st.nextToken());
+//            }
+//
+//            Arrays.sort(grades, ((o1, o2) -> o1[0] - o2[0]));
 
-            int[] result = new int[num];
-            int left = 0;
-            int right = num - 1;
-            for (int j = 0; j < num; j++) {
-                if(j % 2 == 0){
-                    result[left] = arr[j];
-                    left++;
-                }else {
-                    result[right] = arr[j];
-                    right--;
+            int passCnt = 1;
+//            int min = grades[0][1];
+            int min = arr[1];
+//            for (int j = 1; j < cnt; j++) {
+//                int secondScore = grades[j][1];
+//                if (secondScore < min) {
+//                    min = secondScore;
+//                    passCnt++;
+//                }
+//            }
+            for (int j = 2; j <= cnt; j++) {
+                int secondScore = arr[j];
+                if (secondScore < min) {
+                    min = secondScore;
+                    passCnt++;
                 }
             }
 
-            int max = Integer.MIN_VALUE;
-            for (int j = 0; j < num - 1; j++) {
-                max = Math.max(max, Math.abs(result[j] - result[j + 1]));
-            }
-
-            sb.append(max + "\n");
+            result.append(passCnt + "\n");
         }
 
-        out.write(sb.toString());
+        out.write(result.toString());
         out.flush();
         out.close();
     }
