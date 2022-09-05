a2bbc2cc89c86bd3b6e4ba76859a02c384e372b5
최영찬 <gks54410@gmail.com>
2022-09-05 18:31:27 +0900

    Feat: 백준 1965번 풀이
    Signed-off-by: 최영찬 <gks54410@gmail.com>

---
+++ src/basicStep/Problem1965.java
@@ -0,0 +1,34 @@
+package basicStep;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1965 { // 상자 넣기
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine());
+
+        int[] nums = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        System.out.println(result);
+    }
+}
