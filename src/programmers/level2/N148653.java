f57647f6cdb6caa3f6bd4d3dff4099359ec7c192
최영찬 <gks54410@gmail.com>
2023-01-06 19:19:36 +0900

    feat: 마법의 엘리베이터 풀이

---
+++ src/programmers/level2/N148653.java
@@ -0,0 +1,36 @@
+package programmers.level2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class N148653 { // 마법의 엘리베이터
+
+    public static void main(String[] args) {
+        int storey = 16;
+        System.out.println(new Solution().solution(storey));
+    }
+
+    static class Solution {
+        public int solution(int storey) {
+            int save = 0;
+            int answer = 0;
+            char[] storeyChs = String.valueOf(storey).toCharArray();
+            for (int i = storeyChs.length - 1; i >= 0; i--) {
+                int val = (storeyChs[i] - '0') + save;
+
+                save = 0;
+                if (val > 5) {
+                    save = 1;
+                    answer += 10 - val;
+                    continue;
+                } else if (val == 5 && i > 0 && (storeyChs[i - 1] - '0') >= 5) {
+                    storeyChs[i - 1]++;
+                }
+
+                answer += val;
+            }
+
+            return save == 1 ? answer + 1 : answer;
+        }
+    }
+}
