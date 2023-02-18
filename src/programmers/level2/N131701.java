db919f0bdc8b4700cd1e07b4ebcd804775323d01
최영찬 <gks54410@gmail.com>
2023-02-18 11:24:02 +0900

    feat: 연속 부분 수열 합의 개수 풀이 수정

--- src/programmers/level2/N131701.java
+++ src/programmers/level2/N131701.java
@@ -11,32 +11,19 @@ public class N131701 { // 연속 부분 수열 합의 개수
     }
 
     static class Solution {
-        private static Set<Integer> set = new HashSet<>();
-
         public int solution(int[] elements) {
-            calc(1, elements.length, elements);
-            return set.size();
-        }
-
-        private void calc(int depth, int size, int[] elements) {
-            if (depth == size + 1) {
-                return;
+            int size = elements.length;
+            Set<Integer> answer = new HashSet<>();
+            for(int i = 0; i < size; i++) {
+                int idx = i;
+                int sum = 0;
+                for (int j = 0; j < size; j++) {
+                    sum += elements[idx++ % size];
+                    answer.add(sum);
+                }
             }
 
-            int sum = 0;
-            for (int i = 0; i < depth; i++) {
-                sum += elements[i];
-            }
-            set.add(sum);
-
-            for (int i = depth; i < size + depth - 1; i++) {
-                sum -= elements[(i - depth) % size];
-                sum += elements[i % size];
-
-                set.add(sum);
-            }
-
-            calc(depth + 1, size, elements);
+            return answer.size();
         }
     }
 }
