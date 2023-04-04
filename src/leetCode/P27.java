9f112c27c454fcdd897c200cdd5e1467441c26b5
최영찬 <gks54410@gmail.com>
2023-04-04 15:39:34 +0900

    feat: Remove Element 풀이

---
+++ src/leetCode/P27.java
@@ -0,0 +1,24 @@
+package leetCode;
+
+public class P27 { // Remove Element
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        System.out.println(removeElement(nums, val));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right--];
+                continue;
+            }
+            left++;
+        }
+
+        return left;
+    }
+}
